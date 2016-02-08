6d4c0d83e100c300949fb7b892567361c903685c
Qi Zeng <zengq@atlint04.slac.stanford.edu>
2016-02-08 05:40:42 -0800

    some small documentation

--- EventLoop/checkout.sh
+++ EventLoop/checkout.sh
@@ -3,4 +3,6 @@ git clone https://github.com/UCATLAS/xAODAnaHelpers.git
 svn co svn+ssh://qzeng@svn.cern.ch/reps/atlasinst/Institutes/SLAC/AutoHists/trunk AutoHists
 
 # running scripts
+# rcSetup use 2.4.3 to be compatible with 20.7
+# However, we still use 2.3.39 for xAH, which should not matter for now 
 python xAODAnaHelpers/scripts/checkoutASGtags.py 2.3.39
