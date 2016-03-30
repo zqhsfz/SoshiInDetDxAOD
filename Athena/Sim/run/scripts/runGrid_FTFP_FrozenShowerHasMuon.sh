f0059c1b93f963bc9d74076041891d45689be371
Qi Zeng <zengq@atlint03.slac.stanford.edu>
2016-03-30 06:58:25 -0700

    add simulation grid script

---
+++ Athena/Sim/run/scripts/runGrid_FTFP_FrozenShowerHasMuon.sh
@@ -0,0 +1,8 @@
+pathena \
+--trf "Sim_tf.py --conditionsTag 'default:OFLCOND-MC15c-SDR-02' --geometryVersion 'default:ATLAS-R2-2015-03-15-00_VALIDATION' --physicsList FTFP_BERT_LIV_VALIDATION --truthStrategy 'MC12' --simulator 'MC12G4' --DataRunNumber '284500' --DBRelease 'default:current' --postInclude 'default:PyJobTransforms/UseFrontier.py' --preInclude 'EVNTtoHITS:SimulationJobOptions/preInclude.BeamPipeKill.py,SimulationJobOptions/preInclude.FrozenShowers.py' --maxEvents 100 --inputEVNTFile %IN --outputHITSFile %OUT.HITS.pool.root" \
+--nJobs=1000 \
+--nFilesPerJob=1 \
+--skipScout \
+--inDS mc15_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.evgen.EVNT.e3601 \
+--outDS user.qzeng.mc15_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.HITS.e3601_ATLAS-R2-2015-03-15-00.v1_FTFPFrozenShower.try2
+
