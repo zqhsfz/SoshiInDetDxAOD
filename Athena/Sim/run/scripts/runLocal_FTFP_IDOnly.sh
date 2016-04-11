89945734a20aa651b9d3dcb4885d0337d7408755
Qi Zeng <zqhsfz@gmail.com>
2016-04-11 23:32:23 +0200

    add IDOnly simulation scripts

---
+++ Athena/Sim/run/scripts/runLocal_FTFP_IDOnly.sh
@@ -0,0 +1,13 @@
+Sim_tf.py \
+--conditionsTag 'default:OFLCOND-MC15c-SDR-02' \
+--geometryVersion 'default:ATLAS-R2-2015-03-15-00_VALIDATION' \
+--physicsList FTFP_BERT_LIV_VALIDATION \
+--truthStrategy 'MC12' \
+--simulator 'MC12G4' \
+--DataRunNumber '284500' \
+--DBRelease 'default:current' \
+--postInclude 'default:PyJobTransforms/UseFrontier.py' \
+--preInclude 'EVNTtoHITS:SimulationJobOptions/preInclude.BeamPipeKill.py,SimulationJobOptions/preInclude.CaloOffDigitConfig.py,SimulationJobOptions/preInclude.MuonOffDigitConfig.py,SimulationJobOptions/preInclude.ForwardOffDigitConfig.py' \
+--maxEvents 10 \
+--inputEVNTFile /afs/cern.ch/user/q/qzeng/eos/atlas/user/q/qzeng/Pixel/mc15_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.evgen.EVNT.e3601/EVNT.04972716._002051.pool.root.1 \
+--outputHITSFile MyHITS.pool.root
--- Athena/Sim/setup.sh
+++ Athena/Sim/setup.sh
@@ -1,6 +1,8 @@
 setupATLAS
 asetup AtlasProduction,20.7.3.3,here
 export G4RADIOACTIVEDATA=$G4NEUTRONHPDATA/../RadioactiveDecay
+lsetup pyami
+lsetup panda
 
 # for SLAC
-export FRONTIER_SERVER="(serverurl=http://atlasfrontier-ai.cern.ch:8000/atlr)(serverurl=http://lcgft-atlas.gridpp.rl.ac.uk:3128/frontierATLAS)(serverurl=http://frontier-atlas.lcg.triumf.ca:3128/ATLAS_frontier)(serverurl=http://ccfrontier.in2p3.fr:23128/ccin2p3-AtlasFrontier)"
+# export FRONTIER_SERVER="(serverurl=http://atlasfrontier-ai.cern.ch:8000/atlr)(serverurl=http://lcgft-atlas.gridpp.rl.ac.uk:3128/frontierATLAS)(serverurl=http://frontier-atlas.lcg.triumf.ca:3128/ATLAS_frontier)(serverurl=http://ccfrontier.in2p3.fr:23128/ccin2p3-AtlasFrontier)"
